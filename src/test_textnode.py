ffd3d7e02c4703c3d4ff9cd024560e645fd8ea51
jtanquil <jose.tanquiilut@gmail.com>
2024-04-06 16:40:43 -0700

    added tests for split_nodes_delimiter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -43,5 +43,54 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
     self.assertEqual(text_node_to_html_node(self.text_type_image).to_html(), '<img src="https://www.google.com" alt="test">')
     self.assertRaises(ValueError, text_node_to_html_node, self.text_type_invalid)
 
+# test:
+# text node with no inline elements
+# text node with one inline element surrounding the entire text value (ex: "*bold*")
+# text node with inline elements at the beginning/end of the line
+# text node with multiple inline elements
+# text node with a unclosed inline element (ex: "here is `some code") at beginning/mid/end
+# multiple text nodes with each property above ^
+class TestSplitNodesDelimiter(unittest.TestCase):
+  def setUp(self):
+    self.test_cases = {}
+    self.test_results = {}
+    self.exceptions = {}
+
+    self.test_cases["no_inline_elements"] = TextNode("no inline elements", text_type_text)
+    self.test_results["no_inline_elements"] = [TextNode("no inline elements", text_type_text)]
+    
+    self.test_cases["entire_element_inline"] = TextNode("**entire element is inline**", text_type_bold)
+    self.test_results["entire_element_inline"] = [TextNode("entire element is inline", text_type_bold)]
+
+    self.test_cases["beginning_inline"] = TextNode("`beginning` is inline", text_type_code)
+    self.test_results["beginning_inline"] = [TextNode("beginning", text_type_code), TextNode(" is inline", text_type_text)]
+
+    self.test_cases["end_inline"] = TextNode("end is *inline*", text_type_italic)
+    self.test_results["end_line"] = [TextNode("end is ", text_type_text), TextNode("inline", text_type_italic)]
+
+    self.test_cases["multiple_inline"] = TextNode("**multiple** elements **are** in**line**", text_type_bold)
+    self.test_results["multiple_inline"] = [TextNode("multiple", text_type_bold), TextNode(" elements ", text_type_text), TextNode("are", text_type_bold), TextNode(" in", text_type_text), TextNode("line", text_type_bold)]
+
+    self.multiple_nodes = [self.test_cases["no_inline_elements"], self.test_cases["multiple_inline"]]
+    self.multiple_nodes_test_results = self.test_results["no_inline_elements"].extend(self.test_results["multiple_inline"])
+
+    self.exceptions["unclosed_inline_beginning"] = TextNode("`unclosed element", text_type_code)
+    self.exceptions["unclosed_inline_end"] = TextNode("another unclosed element*", text_type_italic)
+    self.exceptions["unclosed_inline_mid"] = TextNode("middle ** unclosed element", text_type_bold)
+
+  def test_split_nodes_delimiter(self):
+    for case in self.test_cases:
+      text_type = self.test_cases[case].text_type
+      result = split_nodes_delimiter([self.test_cases[case]], text_type_delimiters[text_type], text_type)
+      for i in range(len(result)):
+        self.assertEqual(result[i], self.test_results[case][i])
+
+    multiple_nodes_result = split_nodes_delimiter(self.multiple_nodes, text_type_delimiters[text_type_bold], text_type_bold)
+    for i in range(len(multiple_nodes_result)):
+      self.assertEqual(multiple_nodes_result[i], self.multiple_nodes_test_results[i])
+
+    for exception in self.exceptions:
+      self.assertRaises(ValueError, split_nodes_delimiter, self.exceptions[exception])
+
 if __name__ == "__main__":
   unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -6,6 +6,12 @@ text_type_italic = "italic"
 text_type_code = "code"
 text_type_link = "link"
 text_type_image = "image"
+text_type_delimiters = {
+  text_type_text: "",
+  text_type_bold: "**",
+  text_type_italic: "*",
+  text_type_code: "`"
+}
 
 class TextNode():
   def __init__(self, text, text_type, url = None):
@@ -34,3 +40,8 @@ def text_node_to_html_node(text_node):
     return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
   else:
     raise ValueError(f"TextNode type must be one of {text_type_text}, {text_type_bold}, {text_type_italic}, {text_type_code}, {text_type_link}, {text_type_image}.")
+
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+  pass
