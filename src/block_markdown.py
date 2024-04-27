113108fbbc5b1b05154ac1a564477563d0265049
jtanquil <jose.tanquilut@gmail.com>
2024-04-27 01:51:06 -0700

    started markdown_to_block

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,6 +1,19 @@
 import re
 
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
 def markdown_to_blocks(markdown):
   newline_separator = re.compile("\n{2,}")
 
   return list(map(lambda block: block.strip(), re.split(newline_separator, markdown)))
+
+def block_to_block_type(block):
+  heading_match = re.compile("^#{1,6} ")
+  code_match = re.compile("^```.*```$")
+  quote_line_match = re.compile("^>.*")
+  unordered_line_match = re.compile("^(*|-) ")
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import *
 
 class TestMarkdownToBlocks(unittest.TestCase):
   def setUp(self):
@@ -16,6 +16,27 @@ class TestMarkdownToBlocks(unittest.TestCase):
   def test_markdown_to_blocks(self):
     for case in self.test_cases:
       self.assertEqual(markdown_to_blocks(self.test_cases[case]), self.test_results[case])
+
+class TestBlockToBlockType(unittest.TestCase):
+  def setUp(self):
+    self.test_cases = {}
+    self.test_results = {}
+
+    self.test_cases["no_type_paragraph"] = "this is a paragraph"
+    self.test_results["no_type_paragraph"] = block_type_paragraph
+
+    for i in range(1, 7):
+      self.test_cases[f"heading {i}"] = f"{'#' * i} heading"
+      self.test_results[f"heading {i}"] = block_type_heading
+
+    self.test_cases["code"] = "```code snippet```"
+    self.test_cases["code"] = block_type_code
+
+    self.test_cases["quote"] = ">quote\n> more quote"
+    self.test_cases["quote"] = block_type_quote
+
+  def test_block_to_block_type(self):
+    pass
 
 if __name__ == "__main__":
   unittest.main()
